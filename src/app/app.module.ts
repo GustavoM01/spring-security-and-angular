51f0198779a79fb4b99e4a5df280ee4958a335ff
Gustavo Maldonado <gustavo-adolfo.maldonado-ruiz@capgemini.com>
2022-03-21 10:49:53 +0100

    Part 2: ng routes working, protect against csrf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppService } from './app-service.service';
+import { Injectable } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule} from '@angular/common/http'
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { finalize } from 'rxjs/operators';
+import {
+  HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS
+} from '@angular/common/http';
+
+@Injectable()
+export class XhrInterceptor implements HttpInterceptor {
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    const xhr = req.clone({
+      headers: req.headers.set('X-Requested-With', 'XMLHttpRequest')
+    });
+    return next.handle(xhr);
+  }
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [AppService, { provide: HTTP_INTERCEPTORS, useClass: XhrInterceptor, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/main/java/com/gustavo/angularspringsecurity/AngularspringsecurityApplication.java
+++ src/main/java/com/gustavo/angularspringsecurity/AngularspringsecurityApplication.java
@@ -2,9 +2,17 @@ package com.gustavo.angularspringsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -13,12 +21,34 @@ import java.util.UUID;
 @RestController
 public class AngularspringsecurityApplication {
 
+  @RequestMapping("/user")
+  @ResponseBody
+  public Principal user(Principal user) {
+    return user;
+  }
+
   @RequestMapping("/resource")
+  @ResponseBody
   public Map<String, Object> home() {
     Map<String, Object> model = new HashMap<String, Object>();
     model.put("id", UUID.randomUUID().toString());
     model.put("content", "Hello World");
     return model;
+  }
+
+  @Configuration
+  protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      http
+        .httpBasic()
+        .and()
+        .authorizeRequests()
+        .antMatchers("/index.html", "/", "/home", "/login").permitAll()
+        .anyRequest().authenticated()
+        .and().csrf()
+        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
   }
 
 	public static void main(String[] args) {
